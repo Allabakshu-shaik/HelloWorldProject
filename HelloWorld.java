45db5c8baa21d2f3b4b4c176d07735da3da5c847
Allabakshu Shaik <Allabakshu.shaik8@gmail.com>
2021-03-25 15:21:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,9 @@ public class HelloWorld{
      public static void main(String[] args){
           for(int i=1;i<=10;i++){
                System.out.println("Helloworld....................."+i);
-               System.out.println("==================================");
                System.out.println("Helloworld....................."+i);
-               
+               System.out.println("==================================");
+              
             }
 }
 }
